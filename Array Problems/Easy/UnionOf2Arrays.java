b4d7f9286d1d2f93a8513ea44de519255b20a404
ChrisJon02 <chrisjvunnam@gmail.com>
2023-09-06 16:02:07 +0530

    Modified UnionOf2Arrays problem

--- Array Problems/Easy/UnionOf2Arrays.java
+++ Array Problems/Easy/UnionOf2Arrays.java
@@ -6,10 +6,12 @@ public class UnionOf2Arrays {
         int[] arr1 = {1, 2, 3, 4, 6};
         int[] arr2 = {1, 3, 5};
         bruteForce(arr1, arr2, arr1.length, arr2.length);
+        optimal(arr1, arr2, arr1.length, arr2.length);
     }
 
     // Time Complexity: O((m+n)log(m+n))
-    // Space Complexity: O(m+n)
+    // Space Complexity: O(m+n) - if space of union arraylist is considered
+    //                   O(1) - if space of union arraylist is not considered
     static void bruteForce(int arr1[], int arr2[], int n, int m) {
         HashSet <Integer> set =new HashSet<>();
         ArrayList <Integer> list =new ArrayList<>();
@@ -19,5 +21,41 @@ public class UnionOf2Arrays {
             set.add(arr2[i]);
         list.addAll(set);
         System.out.println("Using brute force : " +list);
-}
+    }
+
+    // Time Complexity: O(m+n)
+    // Space Complexity: O(m+n) - if space of union arraylist is considered
+    //                   O(1) - if space of union arraylist is not considered
+    static void optimal(int arr1[], int arr2[], int n, int m){
+        int i = 0, j=0;
+        ArrayList<Integer> arrList = new ArrayList<>();
+        while(i < n && j<m){
+            if(arr1[i] <= arr2[j]){
+                 if(arrList.size() == 0 || arrList.get(arrList.size()-1) != arr1[i]){
+                      arrList.add(arr1[i]);
+                 }
+                 i++;
+            }else{
+                if(arrList.size() == 0 || arrList.get(arrList.size()-1) != arr2[j]){
+                    arrList.add(arr2[j]);
+                }
+                 j++;
+            }
+        }
+
+        while(i < n){
+            if(arrList.get(arrList.size()-1) != arr1[i]){
+                arrList.add(arr1[i]);
+            }
+            i++;
+        }
+        while(j < m){
+            if(arrList.get(arrList.size()-1) != arr2[j]){
+                arrList.add(arr2[j]);
+            }
+            j++;
+        }
+
+        System.out.println("Using Optimal : " +arrList);
+    }
 }
